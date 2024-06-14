c859558a633744349aa7f16498770b67bb81019f
MarisolVitorya <00001156726840sp@al.educacao.sp.gov.br>
2024-06-14 10:18:06 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+    stronke("blue");
+    fill("red")
+
+    1f(mouseisPressed){
+       Reflect(mousex.mousey. 20, 35);
+    }
+    
+  }
